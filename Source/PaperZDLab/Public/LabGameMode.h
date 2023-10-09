90c8c74365827e8d58292a3704146baca6a49446
Bruno Girome <brunogirome@gmail.com>
2023-10-09 00:03:08 -0300

    some changes on blueplrints and gamemode

---
+++ Source/PaperZDLab/Public/LabGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LabGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAPERZDLAB_API ALabGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
